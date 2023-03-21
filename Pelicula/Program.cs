29c5b2ebf26dee417d0aa883ee8570c3ace7c74f
Jaime Alvarez <126440030+Elteclass@users.noreply.github.com>
2023-03-21 01:44:28 +0000

    Prueba de los métodos set y get

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -7,29 +7,39 @@ namespace Pelicula
     public class Pelicula
     {
         //Campos
-        public string titulo;
-        public int año;
-        public string país;
-        public string director;
+        private string titulo;
+        private Int16 año;
+        private string país;
+        private string director;
 
         private List<Actor> actores = new List<Actor>();
         
         //Constructores
-        public Pelicula (string t, int a, string p, string d)
-        {
-            titulo = t;
-            año = a;
-            país = p;
-            director = d;
-        }
      
         //Métodos
+        public void SetTitulo(string t)
+        {
+            titulo = t;
+        }
+
+         public void SetAño(Int16 a)
+        {
+            año = a;
+        }
+        public string GetTitulo ()
+        {
+            return titulo;
+        }
+        public int GetAño ()
+        {
+            return año;
+        }
      
-        public void Imprime()
+        /*public void Imprime()
         {
             //Console.WriteLine($"{titulo} {año}");
             Console.WriteLine("La pelicula es {0}, que se estreno en el año {1}." , titulo , año);
-        }
+        }*/
 
 
     }
@@ -57,10 +67,17 @@ namespace Pelicula
 
         static void Main(string[] args)
         {
+            /*
             Pelicula p1 = new Pelicula("La la land", 2016, "Estados Unidos", "Damien Chazelle");
             Pelicula p2 = new Pelicula("El renacido", 2015, "Estados Unidos", "Alejandro Gonzáles Iñárritu");
             p1.Imprime();
             p2.Imprime();
+            */
+            Pelicula p1 = new Pelicula();
+            p1.SetTitulo("La La Land");
+            p1.SetAño(2016);
+            Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+            
         }
     }
 }
